0fc2bd68bf7e67f72dc0b25cdf85bab5322973d5
Vitaly <you@example.com>
2019-06-21 16:14:24 -0700

    Python problem parser

--- scripts/test_checker.py
+++ scripts/test_checker.py
@@ -1,10 +1,16 @@
 import unittest
 
-from checker import parse_point
+from checker import parse_point, parse_problem
 
 class TestParse(unittest.TestCase):
     def test_parse_point(self):
-        self.assertEqual(parse_point("(1, 1)"), (1, 1))
+        self.assertEqual(parse_point("(1,1)"), (1, 1))
+
+    def test_file(self):
+        path = "../problems/part-1-initial/prob-111.desc"
+        with open(path) as f:
+            s = f.read()
+            return parse_problem(s)
 
 
 if __name__ == '__main__':
