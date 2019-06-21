fdd06dc9ac62661fd3d242f9f7ebe8212480126b
Loks- <alexeypoyarkov@gmail.com>
2019-06-21 14:38:43 -0700

    Set correct number of tests.

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-  for (unsigned i = 1; i <= 5; ++i) {
+  for (unsigned i = 1; i <= 220; ++i) {
     std::string si = std::to_string(i + 1000).substr(1);
     solutions::Solve("../problems/all/prob-" + si + ".desc",
                      "../solutions_cpp/prob-" + si + ".sol");
