fa7e1ca906cdcac68e9a3a524244d47bfbc46de2
Oleg Godovykh <olegg.lieangel@gmail.com>
2019-06-21 15:12:19 -0700

    compress solutions

--- scripts/submit.py
+++ scripts/submit.py
@@ -3,7 +3,9 @@
 from argparse import ArgumentParser, SUPPRESS
 import os
 import sys
+import time
 from types import SimpleNamespace
+from zipfile import ZipFile, ZIP_DEFLATED
 
 from tqdm import tqdm
 from yaml import safe_load
@@ -18,6 +20,8 @@ if __name__ == "__main__":
                         help="Directory with solutions to evaluate")
     parser.add_argument("-p", "--problems", nargs="*",
                         help="List of directories containing problems")
+    parser.add_argument("-s", "--submission",
+                        help="Directory with all existing submissions")
     args = parser.parse_args()
 
     yaml_config.update(vars(args))
@@ -48,6 +52,11 @@ if __name__ == "__main__":
     if not config.evaluate or not os.path.exists(config.evaluate):
         sys.exit("No new problems for evaluation")
 
+    try:
+        os.makedirs(config.submission, exist_ok=True)
+    except:
+        sys.exit("No submission directory exists, and unable to create")
+
     submissions = []
     for problem in tqdm(problems, desc="Comparing solutions"):
         problem_name = os.path.basename(problem)
@@ -56,10 +65,15 @@ if __name__ == "__main__":
         evaluate = os.path.join(config.evaluate, solution_name)
         # TODO: proper evaluation
         if os.path.exists(evaluate) and not os.path.exists(gold):
-            print(f"New solution for {problem_name}")
             submissions.append(evaluate)
 
     print(f"Submitting {len(submissions)} new solutions")
+    timestamp = time.strftime("%Y-%m-%d_%H-%M-%S.zip")
+    archive = os.path.join(config.submission, timestamp)
+    with ZipFile(archive, mode="w", compression=ZIP_DEFLATED, compresslevel=9) as zip:
+        for submission in tqdm(submissions, desc="Archiving new solutions"):
+            zipname = os.path.basename(submission)
+            zip.write(submission, arcname=zipname)
 
     # TODO: submit
     # TODO: merge gold solutions
