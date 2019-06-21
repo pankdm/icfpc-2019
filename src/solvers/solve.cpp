3484a5fa686d61599f4d85cff9e239a4b77628b1
Loks- <alexeypoyarkov@gmail.com>
2019-06-21 16:41:22 -0700

    Add logging to Solve.

--- src/solvers/solve.cpp
+++ src/solvers/solve.cpp
@@ -3,8 +3,10 @@
 #include "base/action_encode.h"
 #include "solvers/base_greedy.h"
 #include "solvers/test.h"
+#include "common/timer.h"
 #include <cassert>
 #include <fstream>
+#include <iostream>
 #include <string>
 
 namespace solvers {
@@ -12,9 +14,16 @@ void Solve(const std::string& input_file, const std::string& output_file) {
   std::ifstream input(input_file);
   std::string task;
   assert(std::getline(input, task));
+  std::string task_index = input_file.substr(input_file.size() - 8, 3);
+  std::cout << "Solving task " << task_index << " ...";
+  Timer t;
   BaseGreedy s;
   auto actions = s.Solve(task);
-  assert(Test(task, actions));
+  std::cout << " Done. Time = " << t.GetMilliseconds() << std::endl;
+  if (!Test(task, actions)) {
+    std::cerr << "Solution for problem " << task_index << " is incorrect."
+              << std::endl;
+  }
   std::ofstream output(output_file);
   output << actions;
 }
