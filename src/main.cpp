ba44d203c4d860f5cf850a3a12eeff2f52886e69
Loks- <alexeypoyarkov@gmail.com>
2019-06-21 17:25:46 -0700

    Add new tasks.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 int main() {
   bool all_ok = true;
   Timer t;
-  for (unsigned i = 1; i <= 220; ++i) {
+  for (unsigned i = 1; i <= 300; ++i) {
     std::string si = std::to_string(i + 1000).substr(1);
     bool b = solvers::Solve("../problems/all/prob-" + si + ".desc",
                             "../solutions_cpp/prob-" + si + ".sol");
