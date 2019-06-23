fd4ad8ddfe4289d8bb658616e09e144ef65ebcf5
kraskoes <krasko.evgeniy@gmail.com>
2019-06-22 23:45:33 -0700

    tmp auto change

--- src/solvers/auto.cpp
+++ src/solvers/auto.cpp
@@ -1,6 +1,7 @@
 #include "solvers/auto.h"
 
 #include "solvers/base_clones.h"
+#include "solvers/base_clones1.h"
 #include "solvers/base_greedy2.h"
 #include "solvers/base_greedy3.h"
 #include "solvers/file.h"
@@ -13,15 +14,17 @@ ActionsClones Auto::Solve(const std::string& task,
   if (task_name == "ext") {
     BaseGreedy2 bg2;
     m.AddSolution(bg2.Solve(task), "bg2");
-    BaseGreedy3 bg3(BaseGreedy3Settings{true, true, 0, 0});
-    m.AddSolution(bg3.Solve(task), "bg3");
     BaseClones bc0;
     m.AddSolution(bc0.Solve(task), "bc0");
+    BaseClones1 bc1;
+    m.AddSolution(bc1.Solve(task), "bc1");
   } else {
     // File fsolver;
     // m.AddSolution(fsolver.Solve(task, task_name), "fsr");
-    BaseGreedy3 bg3(BaseGreedy3Settings{false, true, 2, 2});
-    m.AddSolution(bg3.Solve(task), "bg3");
+    // BaseClones bc0;
+    // m.AddSolution(bc0.Solve(task), "bc0");
+    BaseClones1 bc1;
+    m.AddSolution(bc1.Solve(task), "bc1");
     // for (unsigned i = 0; i < 4; ++i) {
     //   BaseGreedy3 bg3(BaseGreedy3Settings{i & 1, i & 2, 0});
     //   m.AddSolution(bg3.Solve(task), "bg3");
