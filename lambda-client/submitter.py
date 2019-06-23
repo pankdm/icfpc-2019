7b6b9d36737c4417442312a921545a3d529ec8f8
Den Raskovalov <denbox@gmail.com>
2019-06-22 20:11:21 -0700

    fixes

--- lambda-client/submitter.py
+++ lambda-client/submitter.py
@@ -12,15 +12,7 @@ fnameState = "state.json"
 state = {}
 state["submitted"] = []
 
-while True:
-    if os.path.isfile(fnameState):
-        with open(fnameState) as fState:
-            state = loads(fState.read())
-
-    task = subprocess.check_output(
-        [python, "./lambda-cli.py", "getblockinfo"]).decode()
-    # for s in ['balances', 'block', "block_subs", "puzzle", "block_ts", "excluded"]:
-    #     task = task.replace("\'" + s + "\'", "\"" + s + "\"")
+def solve(task):
     task = task.replace("\'", "\"")
     print(task)
     blockinfo = loads(task)
@@ -28,12 +20,12 @@ while True:
     blockId = blockinfo["block"]
 
     if blockId in state["submitted"]:
-        continue
+        return
 
     state["submitted"].append(blockId)
 
-    puzzleIn = "data/%s_puzzle.desc" % blockId
-    puzzleOut = "data/%s_puzzle.res" % blockId
+    puzzleIn = "data/%s_puzzle.desc" % str(blockId)
+    puzzleOut = "data/%s_puzzle.res" % str(blockId)
     with open(puzzleIn, "w") as fTask:
         fTask.write(blockinfo["puzzle"])
 
@@ -43,8 +35,8 @@ while True:
     with open(puzzleOut, "w") as fTemp:
         pass
 
-    taskIn = "data/%s_task.desc" % blockId
-    taskOut = "data/%s_task.res" % blockId
+    taskIn = "data/%s_task.desc" % str(blockId)
+    taskOut = "data/%s_task.res" % str(blockId)
     with open(taskIn, "w") as fTask:
         fTask.write(blockinfo["task"])
 
@@ -52,7 +44,27 @@ while True:
         ["../src/build/cpp_solver", "-solve", "1", "-in", taskIn, "-out", taskOut])
 
     subprocess.check_call([python, "./lambda-cli.py", "submit", "-block",
-                           blockId, "-puzzle_sol_path", puzzleOut, "-task_sol_path", taskOut])
+                           str(blockId), puzzleOut, taskOut])
 
     with open(fnameState, "w") as fState:
         fState.write(dumps(state))
+
+def loadState():
+    if os.path.isfile(fnameState):
+        with open(fnameState) as fState:
+            state = loads(fState.read())
+
+if False:
+    for block in range(1, 13):
+        loadState()
+
+        task = subprocess.check_output(
+            [python, "./lambda-cli.py", "getblockinfo", "-block", str(block)]).decode()
+        solve(task)
+
+while True:
+    loadState()
+
+    task = subprocess.check_output(
+        [python, "./lambda-cli.py", "getblockinfo"]).decode()
+    solve(task)
