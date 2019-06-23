f278b103e676d9271249cb052487cc6fd31eb044
Vitaly <xivaxy@gmail.com>
2019-06-23 00:53:02 -0700

    Add initial boosters to clones_greedy

--- src/solvers/merger.h
+++ src/solvers/merger.h
@@ -8,11 +8,13 @@ class Merger {
  protected:
   std::string task;
   std::string task_name;
+  std::string bonuses;
   unsigned best_score = 0;
   ActionsClones best_solutions;
 
  public:
-  Merger(const std::string& _task, const std::string& _task_name);
+  Merger(const std::string& _task, const std::string& _task_name,
+         const std::string& _bonuses);
   void AddSolution(const ActionsClones& s,
                    const std::string& solution_name = "");
   void AddSolution(const ActionsList& s, const std::string& solution_name = "");
--- src/solvers/solve.cpp
+++ src/solvers/solve.cpp
@@ -16,10 +16,15 @@ unsigned Solve(const std::string& input_file, const std::string& output_file,
   std::ifstream input(input_file);
   std::string task;
   ALWAYS_ASSERT(std::getline(input, task));
+  std::string bonuses;
+  if (bonus_file.size() > 0) {
+    std::ifstream bonus(bonus_file);
+    ALWAYS_ASSERT(std::getline(bonus, bonuses));
+  }
   Timer t;
   Auto s;
-  auto actions = s.Solve(task, task_name);
-  unsigned score = Test(task, actions);
+  auto actions = s.Solve(task, task_name, bonuses);
+  unsigned score = Test(task, actions, bonuses);
   std::cout << "Task " << task_name << " Done. Time = " << t.GetMilliseconds()
             << "\tScore = " << score << std::endl;
   if (score) {
