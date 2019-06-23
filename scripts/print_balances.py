e0fd6894eb4eed5390bf1b39f6b994eff0288ccc
Den Raskovalov <denbox@gmail.com>
2019-06-23 16:27:45 -0700

    new data

--- scripts/print_balances.py
+++ scripts/print_balances.py
@@ -3,13 +3,15 @@
 import json
 import os
 
-for i in range(1, 100):
+for i in range(1, 200):
     filename = "../lambda-client/blocks/%d/balances.json" % i
     if not os.path.isfile(filename):
         continue
     s = open(filename).read()
     balance = 0
     j = json.loads(s)
-    if "15" in j:
-        balance = j["15"]
-    print(i, balance)
+
+pairs = list(j.items())
+pairs = sorted(pairs, key=lambda x: -x[1])
+for k, v in pairs:
+    print("%s\t%s" % (str(k), str(v)))
