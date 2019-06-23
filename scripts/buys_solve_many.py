9192f19f249c1a92af02f5d3a3ae05290a4a6d5e
Dmitry Panin <pankdm@gmail.com>
2019-06-23 13:32:28 -0700

    buys scripts

---
+++ scripts/buys_solve_many.py
@@ -0,0 +1,12 @@
+from buys_solve_one import solve_one
+
+
+boost = ["C", "B"]
+
+# boosters = "CC"
+# boosters = "CB"
+# boosters = "BB"
+
+for i in range(1, 301):
+    for b in boost:
+        solve_one(i, b)
