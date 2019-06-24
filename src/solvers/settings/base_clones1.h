d073ffe6278710736bb894e4f558f1c032c1d418
Den Raskovalov <denbox@gmail.com>
2019-06-23 21:30:16 -0700

    init

--- src/solvers/settings/base_clones1.h
+++ src/solvers/settings/base_clones1.h
@@ -5,12 +5,12 @@
 namespace solvers {
 class BaseClones1Settings {
  public:
-  size_t manip_index;
-  size_t strategy;
-  size_t ext_dist;
-  bool use_shifts;
-  bool even_mops;
-  bool use_teleports;
-  bool all_rotate_and_shift;
+  size_t manip_index{};
+  size_t strategy{};
+  size_t ext_dist{};
+  bool use_shifts{};
+  bool even_mops{};
+  bool use_teleports{};
+  bool all_rotate_and_shift{};
 };
 }  // namespace solvers
