6629b75bd23b09e0e0e6fa30fbdf69b393a3cc42
Dmitry Panin <pankdm@gmail.com>
2019-06-23 19:24:30 -0700

    Add manual solutions

--- scripts/buy_db_analytics.py
+++ scripts/buy_db_analytics.py
@@ -73,29 +73,6 @@ def process_task(n):
     else:
         print (f"{rois[0][1]}")
 
-BALANCE = 339000
-
-def select_manual():
-    os.system("mkdir -p ../solutions/manual/")
-    selected = []
-    for i in range(1, 301):
-        # process_task(i)
-        roi, index = get_best_roi_clone(i)
-        selected.append((roi, i, index))
-    selected.sort(key = lambda x : x[0], reverse=True)
-
-    for i in range(1, 301):
-        os.system(f"cp ../solutions_gold/prob-{i:03}.sol ../solutions_manual/")
-
-    remaining = BALANCE
-    for t in selected:
-        if remaining < 0:
-            break
-        roi, i, index = t
-        remaining -= 2000
-        os.system(f"cp ../buy_db/task{i}/{index}.buy ../solutions_manual/prob-{i:03}.buy")
-        os.system(f"cp ../buy_db/task{i}/{index}.sol ../solutions_manual/prob-{i:03}.sol")
-
 if __name__ == "__main__":
     # select_manual()
     for i in range(1, 301):
