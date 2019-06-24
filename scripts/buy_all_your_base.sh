fef74759c4aebc6f664bab6d4487a16a1ed62e63
Dmitry Panin <pankdm@gmail.com>
2019-06-24 03:44:16 +0000

    fix scripts

---
+++ scripts/buy_all_your_base.sh
@@ -0,0 +1,4 @@
+
+for i in `seq 0 4`; do
+    ./buys_solve_many.py $i >buys.$i.log &
+done
--- scripts/buys_solve_many.py
+++ scripts/buys_solve_many.py
@@ -7,16 +7,32 @@ import sys
 # boosters = ["C", "B"]
 # boosters = ["CC"]
 
-boosters = ["CCC", "CCCC"]
-RANGE = range(1, 221)
+
+
+FULL_RANGE = range(1, 301)
+NON_CLONE_RANGE = range(1, 220)
+
+class BuyTask:
+    def __init__(self, range, boosters):
+        self.range = range
+        self.boosters = boosters
+
+
+tasks = [
+    BuyTask(FULL_RANGE, ("C", "CC")),
+    BuyTask(NON_CLONE_RANGE, ("CCC", "CCCC")),
+]
+
 
 NUM_SHARDS = 5
 shard = int(sys.argv[1])
 assert 0 <= shard < NUM_SHARDS
 
-for i in RANGE:
-    if i % NUM_SHARDS == shard:
-        for b in boosters:
-            print ()
-            print (f"Task {i}, buy {b}")
-            solve_one(i, b)
+
+for task in tasks:
+    for i in task.range:
+        if i % NUM_SHARDS == shard:
+            for b in task.boosters:
+                print ()
+                print (f"Task {i}, buy {b}")
+                solve_one(i, b)
