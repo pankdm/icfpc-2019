36d8055f7ac6b69a1eeeb4ab9f5690b1d7201f28
Dmitry Panin <pankdm@gmail.com>
2019-06-23 19:30:26 -0700

    Add index

--- scripts/buy_db_analytics.py
+++ scripts/buy_db_analytics.py
@@ -9,7 +9,7 @@ FULL_ANALYTICS = True
 
 GOLD = "../solutions_gold/"
 
-def calculate_roi(n, base_yaml, compare_yaml):
+def calculate_roi(n, index, base_yaml, compare_yaml):
     old_score = base_yaml["time"]
     new_score = compare_yaml["time"]
     max_score = compare_yaml["max_score"]
@@ -19,7 +19,7 @@ def calculate_roi(n, base_yaml, compare_yaml):
     spent = compare_yaml["spent"]
     roi = (delta - spent) / spent
     sign = "+" if delta > 0 else "-"
-    msg = (f"Task {n}: {buy} ({spent}): " +
+    msg = (f"Task {n},{index}: {buy} ({spent}): " +
            f"{old_score} -> {new_score} = ({sign}{delta:.1f}), max {max_score}, " +
            f"ROI = {roi * 100:.1f}%")
     # print (msg)
@@ -40,7 +40,7 @@ def get_best_roi_clone(n):
         s = re.match("(\d+).meta.yaml", f)
         if s:
             compare_yaml = read_yaml(folder + f)
-            roi, msg = calculate_roi(n, base_yaml, compare_yaml)
+            roi, msg = calculate_roi(n, s.group(1), base_yaml, compare_yaml)
             if compare_yaml["buy"] != "C":
                 continue
             rois.append((roi, s.group(1)))
@@ -62,7 +62,7 @@ def process_task(n):
         s = re.match("(\d+).meta.yaml", f)
         if s:
             compare_yaml = read_yaml(folder + f)
-            roi, msg = calculate_roi(n, base_yaml, compare_yaml)
+            roi, msg = calculate_roi(n, s.group(1), base_yaml, compare_yaml)
             rois.append((roi, msg))
 
     rois.sort(key = lambda x : x[0], reverse=True)
