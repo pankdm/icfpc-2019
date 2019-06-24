7118b47ee27dc8335f7fae63cd1fcf9c957f0970
Loks- <alexeypoyarkov@gmail.com>
2019-06-24 01:49:33 -0700

    Fix search.

--- src/solvers/worker/local2.cpp
+++ src/solvers/worker/local2.cpp
@@ -4,63 +4,64 @@
 #include "solvers/paths/path_to_target.h"
 #include "common/always_assert.h"
 #include <algorithm>
+#include <unordered_map>
 
 namespace solvers {
 namespace worker {
 void Local2::UpdateTarget() {
+  thread_local std::unordered_map<unsigned, unsigned> rep_to_size;
+  thread_local std::unordered_map<unsigned, unsigned> rep_to_score;
+  thread_local std::queue<std::pair<unsigned, unsigned>> q;
+  thread_local UnsignedSet s;
+  rep_to_size.clear();
+  rep_to_score.clear();
+  for (; !q.empty();) q.pop();
+  if (s.TotalSize() < pworld->Size()) {
+    s.Resize(pworld->Size());
+  }
+  s.Clear();
+
   target.Clear();
   auto w = Get();
-  // score = dist + size
-  int best_score = -1;
-  std::map<int, int> rep_to_score;
-  std::map<int, int> rep_to_size;
   const auto& task = pworld->GetTask(tindex);
+  if (task.Empty()) return;
   for (unsigned u : task.List()) {
     rep_to_size[pworld->DSFind(u)] = pworld->DSSize(u);
   }
-  std::queue<std::pair<int, int>> q;
-  UnsignedSet s(pworld->map.Size());
+
+  // score = dist + size
+  unsigned best_score = unsigned(-1);
+  const unsigned DIST_WEIGHT = 2;
   q.push(std::make_pair(pworld->Index(w.x, w.y), 0));
   s.Insert(pworld->Index(w.x, w.y));
   for (; !q.empty(); q.pop()) {
-    int index = q.front().first;
-    int dist = q.front().second;
-    Point px(pworld->map.X(index), pworld->map.Y(index));
-    if (!pworld->map.Get(px.x, px.y).WrappedOrBlocked()) {
-      int DIST_WEIGHT = 2;
-      if (best_score != -1 && DIST_WEIGHT * dist > best_score) {
-        break;
-      }
-      int rep = pworld->DSFind(index);
-      if (rep_to_score.count(rep) == 0 && task.HasKey(index)) {
-        int score = dist * DIST_WEIGHT + pworld->DSSize(rep);
-        rep_to_score[rep] = score;
-        if (best_score == -1 || score < best_score) {
+    unsigned index = q.front().first;
+    unsigned dist = q.front().second;
+    if (task.HasKey(index)) {
+      unsigned r = pworld->DSFind(index);
+      if (rep_to_score.find(r) == rep_to_score.end()) {
+        unsigned score = dist * DIST_WEIGHT + rep_to_size[r];
+        rep_to_score[r] = score;
+        if (score < best_score) {
           best_score = score;
         }
       }
-      if (rep_to_score.size() == rep_to_size.size()) {
-        break;
-      }
     }
-    for (int i = 0; i < 4; i++) {
-      Point p = px + Direction(i);
-      if (!pworld->map.Inside(p.x, p.y)) {
-        continue;
+    if (dist * DIST_WEIGHT >= best_score) break;
+    for (unsigned u : pworld->GEdges(index)) {
+      if (!s.HasKey(u)) {
+        q.push(std::make_pair(u, dist + 1));
+        s.Insert(u);
       }
-      int index = pworld->map.Index(p.x, p.y);
-      if (s.HasKey(index)) {
-        continue;
-      }
-      q.push(std::make_pair(index, dist + 1));
-      s.Insert(index);
     }
   }
   for (unsigned u : task.List()) {
-    if (rep_to_score[pworld->DSFind(u)] == best_score) {
-      target.Insert(u);
+    auto r = pworld->DSFind(u);
+    if (rep_to_score.find(r) != rep_to_score.end()) {
+      if (rep_to_score[r] == best_score) target.Insert(u);
     }
   }
+  ALWAYS_ASSERT(!target.Empty());
 }
 
 void Local2::Init(unsigned _windex, unsigned _tindex, WorldTaskSplit& world) {
