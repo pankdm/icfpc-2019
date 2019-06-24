71af54da2304889b74feb198b6df516e4774d093
Dmitry Panin <pankdm@gmail.com>
2019-06-24 09:13:30 +0000

    local changes

--- scripts/update_buy_db.sh
+++ scripts/update_buy_db.sh
@@ -2,11 +2,12 @@
 git commit -a -m "local changes"
 
 
-for i in `seq 20 10000`; do
+for i in `seq 23 10000`; do
     git pull --rebase
     git add ../buy_db
-    ./buy_db_analytics.py >roi-full.txt
     git commit -a -m "update buy_db v${i}"
+    ./buy_db_analytics.py >roi-full.txt
+    git commit -a -m "update ROI-full to v${i}"
     git push
     sleep 60
 done
